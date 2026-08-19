0dbf2feba816e593d4f2d20aed7f825507c2cde2
Husen <imhusenbasharasulsab@gmail.com>
2026-08-19 19:11:42 +0530

    Update script.js
    js code updated

--- Web devlopment/Day 23/script.js
+++ Web devlopment/Day 23/script.js
@@ -1,9 +1,41 @@
- alert("Hello World")
+console.log("Hello js");
+console.log("Welcome to javascript");
 
- console.log("Code is running......")
 
- var a=prompt("Enter your name")
+// variables
 
- console.log("Your number "+ a)
+var a=10;
+// var b=25;
+var name="Husen"
+// console.log(a+b);
+// finding type of variable
 
- var b=confirm("are you exiting this page")
+// console.log(typeof a,typeof b,typeof name);
+
+
+{
+    let a=25;
+    console.log(a);
+}
+
+console.log(a);
+
+// datatypes
+
+let A="husen";
+let b=9;
+let c=2.5;
+let d=null;
+let e=undefined;
+
+console.log(A,b,c,d,e);
+console.log(typeof A,typeof b,typeof c,typeof d,typeof e);
+
+// object
+let O={
+    "name":"Husen",
+    "Job code =":9353,
+    is_good:true
+}
+O.salary=10000;
+console.log(O)
