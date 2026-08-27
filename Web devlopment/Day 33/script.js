06e41df8343bf424e5291f1bdf6aeb8999644429
Husen <imhusenbasharasulsab@gmail.com>
2026-08-27 23:30:41 +0530

    Add files via upload

---
+++ Web devlopment/Day 33/script.js
@@ -0,0 +1,7 @@
+let button=document.getElementById("btn")
+
+button.addEventListener("keydown",(e)=>{
+    // alert("Im Clicked")
+    console.log(e)
+    // document.querySelector(".box").innerHTML="<b>Hey i was clickes .!yayyyy! </b>Enjoy your click"
+} )
